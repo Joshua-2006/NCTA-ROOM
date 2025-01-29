2d42acb55867f981e2fd86f22c91a14cde476b50
Joshua Jopling <cookiecattornado@gmail.com>
2025-01-29 10:13:01 -0800

    Update Targe.cs

--- Assets/Josh/Scripts/Targe.cs
+++ Assets/Josh/Scripts/Targe.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Targe : MonoBehaviour
 {
-    public GameObject sp;
-    public GameObject sp2;/*
-    // Start is called before the first frame update
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("Bullet"))
-        {
-            Instantiate(gameObject, sp.transform.position, sp.transform.rotation);
-            Instantiate(gameObject, sp2.transform.position, sp2.transform.rotation);
-        }
-    }*/
+    public int score;
 }
